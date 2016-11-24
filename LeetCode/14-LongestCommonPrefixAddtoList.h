055e4589ff95759c024cfe19643eba73e6b885f5
Jish <ymwtzj@gmail.com>
2016-11-24 16:53:33 +0800

    Jish's laptop 2016-11-24

--- LeetCode/14-LongestCommonPrefixAddtoList.h
+++ LeetCode/14-LongestCommonPrefixAddtoList.h
@@ -7,6 +7,34 @@ using namespace std;
 class Solution {
 public:
 	string longestCommonPrefix(vector<string>& strs) {
+		string ans;
 
+		int k = 0, len = 0;
+		for (size_t i = 0;i < strs.size();i++) {
+			if(strs[i].size()>len){
+				len = strs[i].size();
+				k = i;
+			}
+		}
+
+		for (int j = 0;j < len;j++) {
+			char c = strs[k][j];
+			bool f = true;
+			for(int i=0;i<strs.size();i++){
+				if (strs[i][j] != c) {
+					f = false;
+					break;
+				}
+			}
+
+			if (f) {
+				ans.push_back(c);
+			}
+			else {
+				break;
+			}
+		}
+
+		return ans;
 	}
 };
